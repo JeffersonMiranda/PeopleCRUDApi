cdf19bcdeeef880a1671004e1a2201fcaadd07b8
Jefferson Miranda Carvalho <miranda.w.sam@outlook.com>
2017-12-10 09:59:20 -0300

    CORS_ORIGIN_WHITELIST modified

--- PeopleApi/settings.py
+++ PeopleApi/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['fast-escarpment-66590.herokuapp.com','localhost']
 
 CORS_ORIGIN_WHITELIST  = (
-    'localhost'
+    'localhost:8080'
     )
 
 # Application definition
