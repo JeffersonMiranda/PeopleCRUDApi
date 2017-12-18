fa739804b2ebdd285f7dbafabb8e41f5e9918793
Jefferson Miranda Carvalho <miranda.w.sam@outlook.com>
2017-12-18 18:55:42 -0300

    cors_origin_whitelist changed

--- PeopleApi/settings.py
+++ PeopleApi/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = '847a7b33-17f0-4733-8efd-583c0dac1bbd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['peoplecrud.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['peoplecrudapi.herokuapp.com']
 
 CORS_ORIGIN_WHITELIST  = (
-    'personscrudapi.herokuapp.com'
+    'personscrud.herokuapp.com'
     )
 
 # Application definition
